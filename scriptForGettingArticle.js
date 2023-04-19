db922e4547cde6b05c9db81a873a4b0d9ccea20a
Turaneed <zhanbek.sarsenov@gmail.com>
2023-04-19 18:52:30 +0600

    Adds transferring article Id as parameter to page with full text of article

--- scriptForGettingArticle.js
+++ scriptForGettingArticle.js
@@ -49,7 +49,50 @@ function getMonthByNumber(num) {
     return result;
   }
   
-  let newsList = document.getElementById("news");
+function getMonthByNumber(num) {
+    let result = "";
+    switch (num) {
+      case 0:
+        result = "January";
+        break;
+      case 1:
+        result = "February";
+        break;
+      case 2:
+        result = "March";
+        break;
+      case 3:
+        result = "April";
+        break;
+      case 4:
+        result = "May";
+        break;
+      case 5:
+        result = "June";
+        break;
+      case 6:
+        result = "July";
+        break;
+      case 7:
+        result = "August";
+        break;
+      case 8:
+        result = "September";
+        break;
+      case 9:
+        result = "October";
+        break;
+      case 10:
+        result = "November";
+        break;
+      case 11:
+        result = "December";
+        break;
+    }
+    return result;
+  }
+  
+  let newsList = document.getElementById("article");
   
   console.log(newsList);
   
@@ -60,50 +103,98 @@ function getMonthByNumber(num) {
       return promise.json();
     })
     .then(function (data) {
-      data.results.splice(0, 3).forEach((item) => {
-        console.log(item);
-        const createdDate = new Date(item.updated_date);
+        const articlesArray  = data.results.filter(item => item.short_url === shortURL);        
+        const article = articlesArray[0];       
+        console.log(article);
+        const createdDate = new Date(article.updated_date);
         const day = createdDate.getUTCDate();
         const month =  getMonthByNumber(createdDate.getUTCMonth());
-        const articleId = item.short_url.split('/').pop();
-        console.log(item.short_url);
-        console.log(articleId);
         console.log(createdDate);
         console.log(day);
   
-        const imageUrl = item.multimedia.length > 0 ? item.multimedia[0].url : "";
+        const imageUrl = article.multimedia.length > 0 ? article.multimedia[0].url : "";
   
         newsList.innerHTML += 
-          `<li class = "news-item">
-              <div class = "article">          
-                  <div class = "article-main-part">
-                      <div class = "article-info">
-                          <img class = "avatar" src = "./images/avatar.png">
-                          <span class = "author-name">Authors name</span>
-                          <span class = "in">in</span>
-                          <span class = "topic-name">Topics Name</span>
-                          <span class = "delimeter">·</span>    
-                          <span class = "publish-date">${day} ${month}</span>
-                      </div>    
-                      <a class = "article-topic" href = "./article.html?id=${articleId}">${item.title}</a>    
-                      <a class = "article-summary" href = "./article.html?id=${articleId}">${item.abstract}</a>
-                  </div>
-                  <div class = "article-additional-info">
-                      <div class = "hashtags">
-                          <a class = "hash-button">${item.section}</a>
-                          <span class = "required-time">12 min read</span>
-                          <span class = "delimeter">·</span>      
-                          <span class = "appeal">Selected for you</span>          
-                      </div>
-                      <ul class = "actions-list">
-                          <li class = "actions-item"></li>
-                          <li class = "actions-item"></li>
-                          <li class = "actions-item"></li>
-                      </ul>
+          `                    <div class = "article-info-top">
+          <div class = "article-info-top-left-panel">
+              <img class = "author-avatar" src = "./images/Author-avatar.png" alt = "Author's avatar">
+              <div class = "article-info-container">
+                  <p class = "authors-name">Authors Name</p>
+                  <div class = "article-information">
+                      <span class = "publish-date">${day} ${month}</span>
+                      <span class = "delimeter">·</span> 
+                      <span class = "required-time">12 min read</span>
+                      <span class = "delimeter">·</span>      
+                      <span class = "appeal">Member-only</span>                                     
                   </div>
               </div>
-              <img class = "news-picture" src = "${imageUrl}" alt = "laptop">      
-          </li>`;
-        });
-    })
-    .catch((e) => console.log(e));
+          </div>
+          <div class = "article-info-top-right-panel">
+              <ul class = "social-network-list">
+                  <li class = "social-network-item">
+                      <span class = "visually-hidden">LinkedIn</span>
+                      <span class = "social-network-image linkedin"></span>
+                  </li>
+                  <li class = "social-network-item">
+                      <span class = "visually-hidden">Facebook</span>
+                      <span class = "social-network-image facebook"></span>
+                  </li>
+                  <li class = "social-network-item">
+                      <span class = "visually-hidden">Twitter</span>
+                      <span class = "social-network-image twitter"></span>
+                  </li>
+              </ul>
+          </div>                               
+      </div>
+      <div class = "article-header-container">                        
+          <p class = "main-article-header">
+              7 Practical CSS Tips
+          </p>
+          <p class = "additional-article-header">
+              How product designers can break from the status quo and help our planet
+          </p>                    
+          
+      </div>
+
+      <img class = "article-image" src = "${imageUrl}" alt = "Фото">
+
+      <div class = "article-subheader-container">
+          Subheader
+      </div>
+
+      <div class = "article-text">
+          How long are you awake in the morning before you go online? Perhaps it’s while you’re still lying in bed, using a news feed or social media as the needed stimulant to push you out from under the covers. Or maybe you wait to open your device until after a warm shower and cup of coffee. If you use sleep tracking apps, you might say you never signed off in the first place.
+          And, like millions of others during the pandemic, the internet is probably what enabled you to stay in touch with family, or complete entire years of work and/or school remotely. If this sounds familiar, then you live in a part of the world where an internet connection now counts as an essential utility — one that’s as easy to take for granted as the natural gas heating your shower water or the electricity powering your coffee maker.
+          But if you think we’re hyperconnected today, just wait. Globally, just over 55% of today’s households have an internet connection. This gap between the internet haves and have-nots is referred to as the digital divide, and access is skewed toward richer nations. The gap is projected to close in the next decade as billions of homes connect to the internet for the first time and by 2030 it’s estimated that the technology industry could account for 20% of the global electricity demand. This presents a troublesome dichotomy. On one hand, it supports livelihoods, educations, and bolsters the global economy; on the other hand, the increased usage of the apps, websites, and services that we build will place an even greater strain on our already-overloaded power grids.
+      </div>
+      
+      <div class = "article-info-bottom">
+          <div class = "article-info-bottom-left">
+              <div class = "article-likes-container">
+                  <div class = "article-likes-image-container">
+                      <span class = "article-likes-image"></span>
+                  </div>   
+                  <div class = "article-likes-count-container">
+                      <span class = "article-likes-count">180</span>    
+                  </div>         
+                  
+              </div>
+              <div class = "article-comments-container">
+                  <div class = "article-comments-image-container">
+                      <span class = "article-comments-image"></span>
+                  </div>    
+                  <div class = "article-comments-count-container">
+                      <span class = "article-comments-count">12</span>                                
+                  </div>        
+                  
+              </div>
+          </div>
+          <div class = "article-info-bottom-right">
+              <div class = "article-empty-container">1</div>
+              <div class = "article-bookmark-image-container">
+                  <span class = "article-bookmark-image"></span>                                                      
+              </div>        
+          </div>    
+      </div>`;
+        })
+        .catch((e) => console.log(e));
